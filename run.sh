81a87e75f891a6f430b5c8004193ef52a78964b1
kremenevskiy <kremeneuski@gmail.com>
2022-01-26 21:22:19 +0300

    change

--- run.sh
+++ run.sh
@@ -1,9 +1,14 @@
-echo "Testing..."
+echo "Installing requirements..."
+pip install -r requirements.txt
+echo "Downloading Tiny ImageNet..."
 wget -nc http://cs231n.stanford.edu/tiny-imagenet-200.zip
-unzip tiny-imagenet-200.zip
-rm -r ./tiny-imagenet-200/test
-python3 val_format.py
-find . -name "*.txt" -delete
+echo "Unzipping Tiny ImageNet..."
+unzip -qq tiny-imagenet-200.zip
+echo "Divide Validation folder to Validation / test (50/50) ..."
+python3 prepare_data.py
 mkdir models
+echo "Copying all images to ./tiny-224..."
 cp -r tiny-imagenet-200 tiny-224
+echo "Resizing images to 224 x 224..."
 python3 resize.py
+
